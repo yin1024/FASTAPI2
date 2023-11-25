34302974e8dcc3cd19c86f1b8b1b9289b83838b6
yin1024 <145313943+yin1024@users.noreply.github.com>
2023-11-25 22:44:17 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+web uvicorn app:app --host 0.0.0.0  --port $PORT
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    file = open("name.json", 'r')
+    read_content = file.read()
+    print(read_content)
+    return file
+
+
+if __name__== "__main__":
+    uvicorn.run ("app:app",port=5000,reload=True)
